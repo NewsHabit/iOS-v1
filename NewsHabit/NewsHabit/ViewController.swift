58be90a00b5632f91549c8dec482158e8aefa231
noeyiz <lamoureclatant@gmail.com>
2024-02-01 19:12:20 +0900

    :sparkles: feat: 프로젝트 생성

---
+++ NewsHabit/NewsHabit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsHabit
+//
+//  Created by jiyeon on 2/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
