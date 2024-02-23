fc046fae701d9d3e4db2409b2a09dc1803660bd3
noeyiz <lamoureclatant@gmail.com>
2024-02-24 01:02:50 +0900

    :sparkles: feat: 로딩 상황을 보여주는 프로그레스바 추가

--- NewsHabit/Sources/Presenter/News/NewsView.swift
+++ NewsHabit/Sources/Presenter/News/NewsView.swift
@@ -9,12 +9,19 @@ import UIKit
 import WebKit
 
 import SnapKit
+import Then
 
 class NewsView: UIView {
     
     // MARK: - UI Components
     
     let webView = WKWebView()
+    
+    let progressView = UIProgressView().then {
+        $0.progressViewStyle = .bar
+        $0.tintColor = .label
+        $0.sizeToFit()
+    }
     
     // MARK: - Initializer
     
@@ -31,21 +38,28 @@ class NewsView: UIView {
     
     deinit {
         webView.stopLoading()
+        webView.removeObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress))
     }
-
+    
     // MARK: - Setup Methods
     
     private func setupProperty() {
         webView.navigationDelegate = self
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
     }
     
     private func setupHierarchy() {
         addSubview(webView)
+        addSubview(progressView)
     }
     
     private func setupLayout() {
         webView.snp.makeConstraints {
             $0.edges.equalToSuperview()
+        }
+        
+        progressView.snp.makeConstraints {
+            $0.top.leading.trailing.equalToSuperview()
         }
     }
     
@@ -57,8 +71,26 @@ class NewsView: UIView {
         webView.load(URLRequest(url: url))
     }
     
+    // MARK: - KVO
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "estimatedProgress" {
+            // 애니메이션을 사용하여 프로그레스 바 업데이트
+            UIView.animate(withDuration: 0.3, animations: { [weak self] in
+                guard let self = self else { return }
+                self.progressView.setProgress(Float(self.webView.estimatedProgress), animated: true)
+            })
+        }
+    }
+    
+    
 }
 
 extension NewsView: WKNavigationDelegate {
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        // 로딩 완료 시 프로그레스 바 숨김
+        progressView.isHidden = true
+    }
     
 }
