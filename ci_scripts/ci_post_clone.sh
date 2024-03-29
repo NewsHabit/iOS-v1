5ff817abbe180b2e2faa3a542cb33bf4829a08df
noeyiz <lamoureclatant@gmail.com>
2024-03-29 23:29:24 +0900

    :memo: docs: CI 스크립트 추가

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Tuist 설치
+bash <(curl -Ls https://install.tuist.io)
+
+# ci_scripts 디렉토리를 나와 프로젝트 경로인 NewsHabit로 진입
+cd ..
+
+# fetch를 통해 라이브러리 다운로드
+tuist fetch 
+
+# generate를 통해 .xcworkspace 받아오기
+tuist generate
