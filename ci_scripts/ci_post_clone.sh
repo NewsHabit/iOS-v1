dbd8031968e9daf3f3bc2f53e73f55ad80075303
noeyiz <lamoureclatant@gmail.com>
2024-03-30 10:39:59 +0900

    📝 docs: signing 정보 추가

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -7,7 +7,11 @@ tuist install 3.36.2
 # ci_scripts 디렉토리를 나와 프로젝트 경로인 NewsHabit로 진입
 cd ..
 
+# MASTER_KEY 환경 변수의 값을 master.key 파일에 저장
+echo "$MASTER_KEY" > Tuist/master.key
+
 # Tuist
 tuist clean
-tuist fetch 
+tuist fetch
+tuist signing decrypt
 tuist generate
