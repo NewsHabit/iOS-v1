30141fe77b99f4d7d131d978c61637e5e946d58d
noeyiz <lamoureclatant@gmail.com>
2024-03-30 17:22:51 +0900

    📝 docs: Update ci_post_clone.sh

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# Tuist 3.36.2 설치
+# ✅ 프로젝트 최상단 폴더로 이동
+cd ..
+
+# ✅ Tuist 설치
 bash <(curl -Ls https://install.tuist.io)
 tuist install 3.36.2
 
-# ci_scripts 디렉토리를 나와 프로젝트 경로인 NewsHabit로 진입
-cd ..
-
-# MASTER_KEY 환경 변수의 값을 master.key 파일에 저장
+# ✅ MASTER_KEY 환경 변수의 값을 master.key 파일에 저장
 echo "$MASTER_KEY" > Tuist/master.key
 
-# Tuist
+# ✅ 의존성 설치, 인증서 및 프로비저닝 프로파일 복호화, xcworkspace 생성
 tuist clean
 tuist fetch
 tuist signing decrypt
