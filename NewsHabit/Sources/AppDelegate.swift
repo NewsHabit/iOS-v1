e29a54c6cb8c248a037392f6c63737e5fe8fdc5a
noeyiz <lamoureclatant@gmail.com>
2024-03-31 13:46:09 +0900

    🐛 fix: 이 달의 기록 오늘 표시 문제

--- NewsHabit/Sources/AppDelegate.swift
+++ NewsHabit/Sources/AppDelegate.swift
@@ -11,11 +11,6 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // 이번 달 기록 설정
-        if UserDefaultsManager.lastMonth != Date().toMonthString() {
-            UserDefaultsManager.lastMonth = Date().toMonthString()
-            UserDefaultsManager.monthlyAllRead = []
-        }
         return true
     }
     
