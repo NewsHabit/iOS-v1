985591d0e7dfeb7c6015a6aa41f46ac32656adcc
noeyiz <lamoureclatant@gmail.com>
2024-04-21 19:23:18 +0900

    :sparkles: feat: Kingfisher를 사용해 이미지 로드

--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -3,7 +3,8 @@ import ProjectDescription
 let spm = SwiftPackageManagerDependencies([
     .remote(url: "https://github.com/SnapKit/SnapKit.git", requirement: .upToNextMinor(from: "5.0.1")),
     .remote(url: "https://github.com/devxoul/Then", requirement: .upToNextMajor(from: "2")),
-    .remote(url: "https://github.com/Alamofire/Alamofire.git", requirement: .upToNextMajor(from: "5.8.1"))
+    .remote(url: "https://github.com/Alamofire/Alamofire.git", requirement: .upToNextMajor(from: "5.8.1")),
+    .remote(url: "https://github.com/onevcat/Kingfisher.git", requirement: .upToNextMajor(from: "7.0.0"))
 ])
 
 let dependencies = Dependencies(
