c6701e82c3a257100442be55b6382ca26c544ceb
noeyiz <lamoureclatant@gmail.com>
2024-04-28 17:28:00 +0900

    :ringed_planet: build: Tuist 설정 변경

--- Project.swift
+++ Project.swift
@@ -1,68 +1,53 @@
 import ProjectDescription
 
-// MARK: - ProjectFactory Protocol
+let projectName: String = "NewsHabit"
 
-protocol ProjectFactory {
-    var projectName: String { get }
-    var dependencies: [TargetDependency] { get }
-    
-    func generateTarget() -> [Target]
-}
+let bundleId: String = "com.NewsHabit"
 
-// MARK: - NewsHabitFactory
-
-class NewsHabitFactory: ProjectFactory {
-    
-    let projectName: String = "NewsHabit"
-    let bundleId: String = "com.NewsHabit"
-    
-    let dependencies: [TargetDependency] = [
-        .external(name: "SnapKit"),
-        .external(name: "Then"),
-        .external(name: "Alamofire"),
-        .external(name: "Kingfisher")
-    ]
-    
-    let infoPlist: [String: Plist.Value] = [
-        "ITSAppUsesNonExemptEncryption": .boolean(false),
-        "CFBundleDisplayName": .string("뉴빗"),
-        "CFBundleName": .string("NewsHabit"),
-        "CFBundleShortVersionString": .string("1.0.2"),
-        "CFBundleVersion": .string("1"),
-        "UILaunchStoryboardName": .string("LaunchScreen"),
-        "UIApplicationSceneManifest": .dictionary([
-            "UIApplicationSupportsMultipleScenes": .boolean(false),
-            "UISceneConfigurations": .dictionary([
-                "UIWindowSceneSessionRoleApplication": .array([
-                    .dictionary([
-                        "UISceneConfigurationName": .string("Default Configuration"),
-                        "UISceneDelegateClassName": .string("$(PRODUCT_MODULE_NAME).SceneDelegate")
-                    ])
+let infoPlist: [String: Plist.Value] = [
+    "ITSAppUsesNonExemptEncryption": .boolean(false),
+    "CFBundleDisplayName": .string("뉴빗"),
+    "CFBundleName": .string("NewsHabit"),
+    "CFBundleShortVersionString": .string("1.0.2"),
+    "CFBundleVersion": .string("1"),
+    "UILaunchStoryboardName": .string("LaunchScreen"),
+    "UIApplicationSceneManifest": .dictionary([
+        "UIApplicationSupportsMultipleScenes": .boolean(false),
+        "UISceneConfigurations": .dictionary([
+            "UIWindowSceneSessionRoleApplication": .array([
+                .dictionary([
+                    "UISceneConfigurationName": .string("Default Configuration"),
+                    "UISceneDelegateClassName": .string("$(PRODUCT_MODULE_NAME).SceneDelegate")
                 ])
             ])
-        ]),
-    ]
-    
-    func generateTarget() -> [ProjectDescription.Target] {[
-        Target(
-            name: projectName,
-            destinations: [.iPhone],
-            product: .app,
-            bundleId: bundleId,
-            deploymentTargets: .iOS("15.0"),
-            infoPlist: .extendingDefault(with: infoPlist),
-            sources: ["\(projectName)/Sources/**"],
-            resources: "\(projectName)/Resources/**",
-            dependencies: dependencies
-        )
-    ]}
-}
+        ])
+    ]),
+]
 
 // MARK: - Project
 
-let factory = NewsHabitFactory()
-
 let project: Project = .init(
-    name: factory.projectName,
-    targets: factory.generateTarget()
+    name: projectName,
+    packages: [
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.8.1")),
+        .package(url: "https://github.com/onevcat/Kingfisher.git", .upToNextMajor(from: "7.0.0")),
+        .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMinor(from: "5.0.1")),
+        .package(url: "https://github.com/devxoul/Then", .upToNextMajor(from: "2.0.0")),
+    ],
+    targets: [Target(
+        name: projectName,
+        destinations: [.iPhone],
+        product: .app,
+        bundleId: bundleId,
+        deploymentTargets: .iOS("15.0"),
+        infoPlist: .extendingDefault(with: infoPlist),
+        sources: ["\(projectName)/Sources/**"],
+        resources: "\(projectName)/Resources/**",
+        dependencies: [
+            .package(product: "Alamofire"),
+            .package(product: "Kingfisher"),
+            .package(product: "SnapKit"),
+            .package(product: "Then")
+        ]
+    )]
 )
