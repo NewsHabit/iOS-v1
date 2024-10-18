1795ff1786fd65d88df04e37725f9f4f244871bb
noeyiz <lamoureclatant@gmail.com>
2024-10-19 05:11:51 +0900

    feat: SelectableButton 구현
    - 버튼 선택 여부에 따라 UI가 변하는 버튼 구현

--- Projects/App/Sources/ViewController.swift
+++ Projects/App/Sources/ViewController.swift
@@ -8,101 +8,30 @@
 import UIKit
 
 import Shared
+import SnapKit
 
 class ViewController: UIViewController {
-    private let scrollView: UIScrollView = {
-        let scrollView = UIScrollView()
-        scrollView.translatesAutoresizingMaskIntoConstraints = false
-        return scrollView
-    }()
-    
-    private let stackView: UIStackView = {
-        let stackView = UIStackView()
-        stackView.axis = .vertical
-        stackView.alignment = .center
-        stackView.spacing = 20
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        return stackView
+    let button = {
+        let button = SelectableButton(title: "버튼")
+        button.clipsToBounds = true
+        button.layer.cornerRadius = 8
+        return button
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = .white
-        setupScrollView()
-        addImages()
-    }
-    
-    private func setupScrollView() {
-        view.addSubview(scrollView)
-        scrollView.addSubview(stackView)
-        
-        NSLayoutConstraint.activate([
-            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
-            
-            stackView.topAnchor.constraint(equalTo: scrollView.topAnchor),
-            stackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
-            stackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
-            stackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
-            stackView.widthAnchor.constraint(equalTo: scrollView.widthAnchor)
-        ])
-    }
-    
-    private func addImages() {
-        let imageProperties: [String: UIImage] = [
-            "back": Images.back,
-            "bookmarkActive": Images.bookmarkActive,
-            "bookmarkInactive": Images.bookmarkInactive,
-            "checkActive": Images.checkActive,
-            "checkInactive": Images.checkInactive,
-            "chevronLeft": Images.chevronLeft,
-            "chevronRight": Images.chevronRight,
-            "empty": Images.empty,
-            "error": Images.error,
-            "export": Images.export,
-            "homeActive": Images.homeActive,
-            "homeInactive": Images.homeInactive,
-            "logoBelt": Images.logoBelt,
-            "newsActive": Images.newsActive,
-            "newsInactive": Images.newsInactive,
-            "refresh": Images.refresh,
-            "settingsActive": Images.settingsActive,
-            "settingsInactive": Images.settingsInactive
-        ]
-        
-        for (name, image) in imageProperties {
-            let containerView = UIView()
-            containerView.translatesAutoresizingMaskIntoConstraints = false
-            
-            let imageView = UIImageView(image: image)
-            imageView.contentMode = .scaleAspectFit
-            imageView.translatesAutoresizingMaskIntoConstraints = false
-            
-            let label = UILabel()
-            label.text = name
-            label.textAlignment = .center
-            label.translatesAutoresizingMaskIntoConstraints = false
-            
-            containerView.addSubview(imageView)
-            containerView.addSubview(label)
-            
-            NSLayoutConstraint.activate([
-                imageView.topAnchor.constraint(equalTo: containerView.topAnchor),
-                imageView.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
-                imageView.widthAnchor.constraint(equalToConstant: 100),
-                imageView.heightAnchor.constraint(equalToConstant: 100),
-                
-                label.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 8),
-                label.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
-                label.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
-                label.bottomAnchor.constraint(equalTo: containerView.bottomAnchor)
-            ])
-            
-            stackView.addArrangedSubview(containerView)
+        view.backgroundColor = Colors.background
+        view.addSubview(button)
+        button.snp.makeConstraints { make in
+            make.left.right.equalToSuperview().inset(20)
+            make.height.equalTo(56)
+            make.center.equalToSuperview()
         }
+        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+    }
+    
+    @objc func buttonTapped(_ button: UIButton) {
+        button.isSelected.toggle()
     }
 }
 
