cd733a3c73a62005c34d5b710c983827db4343f5
noeyiz <lamoureclatant@gmail.com>
2024-10-20 01:02:20 +0900

    feat: NewsCountView UI 구현
    - 기존 SelectableButton을 CategoryCell로 변경
    - 기존 CheckBoxbutton을 NewsCountCell로 변경

---
+++ Projects/Shared/Util/Sources/Models/NewsCount.swift
@@ -0,0 +1,14 @@
+//
+//  NewsCount.swift
+//  SharedUtil
+//
+//  Created by 지연 on 10/20/24.
+//
+
+import Foundation
+
+public enum NewsCount: Int, CaseIterable, Codable {
+    case three = 3
+    case four = 4
+    case five = 5
+}
