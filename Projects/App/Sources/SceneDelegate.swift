0e3f89d9127e1dad9176fd3e781874c3b9d6233b
noeyiz <lamoureclatant@gmail.com>
2024-10-20 01:10:14 +0900

    feat: TabBarController UI 구현

--- Projects/App/Sources/SceneDelegate.swift
+++ Projects/App/Sources/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     ) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = UINavigationController(rootViewController: MyViewController())
+        window?.rootViewController = UINavigationController(rootViewController: TabBarController())
         window?.makeKeyAndVisible()
     }
 }
---
+++ Projects/App/Sources/TabBarController.swift
@@ -0,0 +1,59 @@
+//
+//  TabBarController.swift
+//  newshabit
+//
+//  Created by 지연 on 10/20/24.
+//
+
+import UIKit
+
+import Shared
+
+final class TabBarController: UITabBarController {
+    private enum Tab: CaseIterable {
+        case home
+        case hot
+        case settings
+        
+        var activeImage: UIImage {
+            switch self {
+            case .home:     Images.homeActive
+            case .hot:      Images.newsActive
+            case .settings: Images.settingsActive
+            }
+        }
+        
+        var inactiveImage: UIImage {
+            switch self {
+            case .home:     Images.homeInactive
+            case .hot:      Images.newsInactive
+            case .settings: Images.settingsInactive
+            }
+        }
+    }
+    
+    // MARK: - Lifecycle
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        setupTabBar()
+    }
+    
+    // MARK: - Setup Methods
+    
+    private func setupTabBar() {
+        tabBar.backgroundColor = Colors.background
+        tabBar.tintColor = Colors.gray09
+        tabBar.unselectedItemTintColor = Colors.gray09
+        
+        viewControllers = Tab.allCases.map { tab in
+            let viewController = ViewController()
+            viewController.tabBarItem = UITabBarItem(
+                title: nil,
+                image: tab.inactiveImage,
+                selectedImage: tab.activeImage
+            )
+            return UINavigationController(rootViewController: viewController)
+        }
+    }
+}
